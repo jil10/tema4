cfa2c8124434985ac972f1b587edeafa2467cc1a
jil10 <45973253+jil10@users.noreply.github.com>
2019-01-16 17:46:41 +0100

    # WARNING: head commit changed in the meantime
    version 1.0

--- src/sopa_de_letras.java
+++ src/sopa_de_letras.java
@@ -5,31 +5,29 @@ public class sopa_de_letras {
 	public static char s[][] = new char[15][15];
 	static Scanner t = new Scanner(System.in);
 
-		
-
-	
-	
-	
 	/*
 	 * METODO: public static void posicion().
 	 * 
 	 * 
-	 * DESCRIPCIÓN: En este metodo lo que hace es que pide la palabra y te la coloca aleatoriamente dentro de la sopa de letras.
+	 * DESCRIPCIÓN: En este metodo lo que hace es que pide la palabra y te la coloca
+	 * aleatoriamente dentro de la sopa de letras.
 	 */
 	public static void posicion() {
 
-		int f, c, i, k, l,con = 0;
+		int f, c, i, k, l, con = 0;
 		String pal = null;
-		
+
+		System.out.println("Introduce las palabras que quieres que esten en la sopa de letra :");
 		do {
 			boolean ok = true;
+			boolean print = true;
 
 			try {
 				do {
-					System.out.println("Introduce las palabras que quieres que esten en la sopa de letra :");
+					System.out.println((con + 1) + " :");
 					pal = t.next();
 					pal = pal.toUpperCase();
-				} while ((pal.length() < 3) || (pal.length() > 15));
+				} while ((pal.length() < 3) || (pal.length() > 15) || (pal.matches(".*[^A-Z].*")));
 
 			} catch (Exception e) {
 				t.next();
@@ -39,110 +37,171 @@ public class sopa_de_letras {
 			char pal1[] = pal.toCharArray();
 
 			for (i = 0; i < pal.length(); i++) {
-				System.out.println(pal1[i]);
+
 			}
 
 			do {
-				
+				ok = true;
+				print = true;
 				try {
-				f = (int) (Math.random() * 15);
-				c = (int) (Math.random() * 15);
+					f = (int) (Math.random() * 15);
+					c = (int) (Math.random() * 15);
 
-				s[f][c] = pal1[0];
+					s[f][c] = pal1[0];
 
-				int opc = (int) (Math.random() * (8 - 1) + 1);
+					if (s[f][c] == pal1[0]) {
 
-				switch (opc) {
-
-				
-				case 1: // Diagonal a la derecha hacia arriba 
-					for (int j = 1; j < pal.length(); j++) {
-						s[f + j][c + j] = pal1[j];
-
-						if (((f + j) > 15) || ((c + j) > 15)) {
-							ok = false;
-						}
+					} else {
+						print = false;
 					}
-					break;
 
-				case 2:// Vertical hacia abajo.
-					for (int j = 1; j < pal.length(); j++) {
-						s[f + j][c] = pal1[j];
-						
-						if ((f+j)>15) {
-							ok = false;
-						}
-					}
-					break;
+					int opc = (int) (Math.random() * (8 - 1) + 1);
 
-				case 3:// Horizontal de izquierda a derecha.
-					for (int j = 1; j < pal.length(); j++) {
-						s[f][c + j] = pal1[j];
-						
-						if((c+j)>15) {
-							ok = false;
-						}
-					}
-					break;
+					switch (opc) {
 
-				case 4://Vertical hacia arriba .
-					for (int j = 1; j < pal.length(); j++) {
-						s[f - j][c] = pal1[j];
-						
-						if ((f+j)<0) {
-							ok = false;
-						}
-					}
-					break;
+					case 1: // Diagonal a la derecha hacia abajo
 
-				case 5:// Horizontal hacia la izquierda.
-					for (int j = 1; j < pal.length(); j++) {
-						s[f][c - j] = pal1[j];
-						
-						if ((c+j)<0) {
+						if (((f + (pal.length() - 1)) > 15) || ((c + (pal.length() - 1)) > 15)) {
 							ok = false;
-						}
-					}
-					break;
-					
-				case 6: // Diagonal hacia la derecha y hacia arriba.
-					for (int j = 1; j < pal.length(); j++) {
-						s[f - j][c + j] = pal1[j];
-						
-						if (((f+j)<0) || ((c+j)> 15 )) {
-							ok = false;
-						}
-					}
-					break;
+						} else {
 
-				case 7: // Diagonal hacia la izquierda y hacia abajo 
-					for (int j = 1; j < pal.length(); j++) {
-						s[f + j][c - j] = pal1[j];
-						
-						if (((f+j)>15) || ((c+j)<0)) {
-							ok = false;
-						}
-					}
-					break;
+							for (int j = 1; j < pal.length(); j++) {
+								s[f + j][c + j] = pal1[j];
 
-				case 8: // Diagonal hacia la izquierda y hacia arriba. 
-					for (int j = 1; j < pal.length(); j++) {
-						s[f - j][c - j] = pal1[j];
-						
-						if (((f+j)<0) || ((c+j)<0)) {
+								if (s[f + j][c + j] == pal1[j]) {
+
+								} else {
+									print = false;
+								}
+							}
+						}
+						break;
+
+					case 2:// Vertical hacia abajo.
+
+						if ((f + (pal.length() - 1)) > 15) {
+							ok = false;
+						} else {
+							for (int j = 1; j < pal.length(); j++) {
+								s[f + j][c] = pal1[j];
+
+								if (s[f + j][c] == pal1[j]) {
+
+								} else {
+									print = false;
+								}
+							}
+						}
+						break;
+
+					case 3:// Horizontal de izquierda a derecha.
+
+						if ((c + (pal.length() - 1)) > 15) {
+							ok = false;
+						} else {
+							for (int j = 1; j < pal.length(); j++) {
+								s[f][c + j] = pal1[j];
+
+								if (s[f][c + j] == pal1[j]) {
+
+								} else {
+									print = false;
+								}
+							}
+						}
+						break;
+
+					case 4:// Vertical hacia arriba .
+
+						if ((f - (pal.length() - 1)) < 0) {
+							ok = false;
+						} else {
+
+							for (int j = 1; j < pal.length(); j++) {
+								s[f - j][c] = pal1[j];
+
+								if (s[f - j][c] == pal1[j]) {
+
+								} else {
+									print = false;
+								}
+							}
+						}
+						break;
+
+					case 5:// Horizontal hacia la izquierda.
+
+						if ((c - (pal.length() - 1)) < 0) {
+							ok = false;
+						} else {
+							for (int j = 1; j < pal.length(); j++) {
+								s[f][c - j] = pal1[j];
+
+								if (s[f][c - j] == pal1[j]) {
+
+								} else {
+									print = false;
+								}
+							}
+						}
+						break;
+
+					case 6: // Diagonal hacia la derecha y hacia arriba.
+
+						if (((f - (pal.length() - 1)) < 0) || ((c + (pal.length() - 1)) > 15)) {
+							ok = false;
+						} else {
+							for (int j = 1; j < pal.length(); j++) {
+								s[f - j][c + j] = pal1[j];
+
+								if (s[f - j][c + j] == pal1[j]) {
+
+								} else {
+									print = false;
+								}
+							}
+						}
+						break;
+
+					case 7: // Diagonal hacia la izquierda y hacia abajo
+
+						if (((f + pal.length() - 1) > 15) || (c - (pal.length() - 1) < 0)) {
+							ok = false;
+						} else {
+							for (int j = 1; j < pal.length(); j++) {
+								s[f + j][c - j] = pal1[j];
+
+								if (s[f - j][c] == pal1[j]) {
+
+								} else {
+									print = false;
+								}
+							}
+						}
+						break;
+
+					case 8: // Diagonal hacia la izquierda y hacia arriba.
+
+						if (((f - pal.length() - 1) < 0) || ((c - pal.length() - 1) < 0)) {
 							ok = true;
+						} else {
+							for (int j = 1; j < pal.length(); j++) {
+								s[f - j][c - j] = pal1[j];
+
+								if (s[f - j][c - j] == pal1[j]) {
+
+								} else {
+									print = false;
+								}
+							}
 						}
+						break;
 					}
-					break;
+				} catch (Exception e) {
+
 				}
-				}
-				catch (Exception e) {
-				
-					
-					
-				}
-			} while (ok == false);
-			
+			} while ((ok == false) || (print == false));
+
 		} while (con < 10);
 
 	}
@@ -151,30 +210,28 @@ public class sopa_de_letras {
 	 * METODO: public static void sopa(). ENTRADAS: SALIDAS: DESCRIPCIÓN:
 	 */
 
-	public static void sopa () {
+	public static void sopa() {
 		int f, c;
-		
-		for(f =0; f<15 ; f++) {
-			for (c=0;c<15;c++) {
-				s [f][c] = (char) (Math.random()* 26 + 'A');
-				
+
+		for (f = 0; f < 15; f++) {
+			for (c = 0; c < 15; c++) {
+				s[f][c] = (char) (Math.random() * 26 + 'a');
+
 			}
 			System.out.println();
 		}
 		System.out.println();
-		
-		
-			posicion();
-		
-		
-		for(f =0; f<15 ; f++) {
-			for (c=0;c<15;c++) {
-				
+
+		posicion();
+
+		for (f = 0; f < 15; f++) {
+			for (c = 0; c < 15; c++) {
+
 				System.out.print(" " + s[f][c]);
 			}
 			System.out.println();
 		}
-	
+
 	}
 
 	/*
